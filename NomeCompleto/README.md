7ae1a519aa687271c601f68fb0a98325a878726a
amandaxavierdeaguiar <amandaxavierdeaguiar@gmail.com>
2023-02-23 17:38:23 +0000

    Aula sobre métodos e variáveis

---
+++ NomeCompleto/README.md
@@ -0,0 +1,1 @@
+Exercícios realizados na aula de Java sobre métodos e variáveis ministrada pelo Professor Gleyson Sampaio.
---
+++ NomeCompleto/src/NomeCompleto.java
@@ -0,0 +1,14 @@
+public class NomeCompleto {
+    public static void main(String[] args) {
+        String primeiroNome = "Amanda";
+        String segundoNome = "Xavier";
+        String terceiroNome = "Aguiar";
+
+        String nomeApelido = nomeApelido (primeiroNome, segundoNome, terceiroNome);
+
+        System.out.println(nomeApelido);
+    }
+    public static String nomeApelido (String primeiroNome, String segundoNome, String terceiroNome){
+    return "Meu nome completo é:" + primeiroNome + " " + segundoNome + " " + terceiroNome;
+    }
+}
