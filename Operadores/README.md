a9e7afe084f5ef83f81dd7ed23bd15fec77b4d2a
amandaxavierdeaguiar <amandaxavierdeaguiar@gmail.com>
2023-02-23 21:01:37 +0000

    Operador Ternario

---
+++ Operadores/README.md
@@ -0,0 +1,1 @@
+Exercício sobre a condição ternária que é uma forma resumida para definir uma condição e escolher por um dentre dois valores.
---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 7;
+        b = 9;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+        /* Exemplo sem a forma reduzida, utilizando if/else
+        String resultado = ""; (retirar esse caso utilize a opção reduzida)
+        if (a == b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+         */
+        
+        System.out.println (resultado);
+    }
+}
